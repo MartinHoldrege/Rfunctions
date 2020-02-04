e1ae5c1372ce57ac086bd36ed608e8306fd9afa7
MartinHoldrege <martinholdrege@gmail.com>
2020-02-03 21:45:40 -0700

    clark fun update

--- assign_trmts_functions.r
+++ assign_trmts_functions.r
@@ -3,14 +3,19 @@
 #and clarkston.
 
 #creating vector of the trmts based on vector of plot numbers at clarkston
-trmts_clark <- function(x){
+trmts_clark <- function(x, convert2mm = FALSE, dump_label = FALSE){
     stopifnot(is.numeric(x),
               all(x %in% 1:14))
     lookup <- c("1"=3, "2"=1,"3"=3,"4"=0,"5"=0,"6"="cc","7"=-1,"8"=2,
                 "9"=5,"10"="cc","11"="cc","12"=10,"13"=0,"14"=3)
     lookup <- factor(lookup, ordered =TRUE,levels=c("cc","-1","0","1","2","3","5","10"))
     x <- as.character(x)
-    unname(lookup[x])
+    out <- unname(lookup[x])
+    if (convert2mm | dump_label) {
+        # not sure why this works--recursion--c2mm_HWRanch calls trmts_clark
+        out <- c2mm_clark(out, dump_label = dump_label)
+    }
+    out
  }
 
 #creating vector of the trmts based on vector of plot numbers at Hardware
@@ -148,4 +153,63 @@ if (FALSE) {
     lohi_HWRanch(1:14)
     lohi_HWRanch(c(1, 2, 3, 4, 6, 7, 9, 10, 11))
     lohi_HWRanch(c(3, 5, 8, 12, 13, 14))
+}
+
+
+# c 2 mm trmts clark ------------------------------------------------------
+
+# convert trmt levels from deg C to mm ------------------------------------
+
+c2mm_clark <- function(x, dump_label = FALSE) {
+    # args:
+    #   x--vector of treatment levels (old deg C labeling)
+    #   return labels of the dump size instead
+    # returns:
+    #   vector of treatment levels converted to mm precip (mean event sizes)
+    #       or trmt label (dump size, mm)
+    
+    stopifnot(
+        is.logical(dump_label)
+    )
+    
+    x_char <- as.character(x)
+    
+    if (!dump_label) {
+        # mean event sizes (from tipping bucket applied to non-winter months)
+        lookup_nocc <- c("-1" = 4.8, "0" = 5.3, "1" = 6.2, "2" = 7.2, "3" = 8.4, "5" = 10.8, 
+                         "10" = 19.4)
+    } else {
+        lookup_nocc <- c("-1" = "1 mm", "0" = 'Control', "1" = "2 mm", 
+                         "2" = "3 mm", "3" = "4 mm", "5" = "8 mm", 
+                         "10" = "18 mm")
+    }
+    
+    if(is.numeric(x)){
+        stopifnot(all(x %in% c(-1, 0, 1, 2, 3, 5, 10)))
+        out <- lookup_nocc[x_char]
+        
+    } else if(is.factor(x)) {
+        stopifnot(
+            all(x_char %in% as.character(trmts_clark(1:14)))
+        )
+        lookup_all <- c("cc" = "cc", lookup_nocc)
+        lookup_all <- factor(lookup_all, levels = unname(lookup_all), ordered = TRUE)
+        out <- lookup_all[x_char]
+    } else {
+        stop("x must be numeric or factor")
+    }
+    out
+}
+
+# test
+if (FALSE) {
+    x <- trmts_clark(1:14) 
+    c2mm_clark(x)
+    c2mm_clark(x, dump_label = TRUE)
+    trmts <- c(-1, 0, 1, 2, 3, 5, 10)
+    c2mm_clark(trmts)
+    c2mm_clark(trmts, dump_label = TRUE)
+    # ~~~~~~~~
+    c2mm_clark(c(-1, 2.5, 3)) # shouldn't run
+    trmts_clark(1:14, convert2mm = TRUE)
 }
